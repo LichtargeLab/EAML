fa419c711206c0a5393f47164406c15c9f24ee37
Dillon Shapiro <drshapir@bcm.edu>
2019-09-24 13:50:56 -0500

    moved experiment dir argument to each subparser to fix help not printing

--- ea_ml/cli.py
+++ ea_ml/cli.py
@@ -18,30 +18,31 @@ from .visualize import visualize
 def main(args=None, function=None):
     """Process command-line arguments and run the program."""
     parser = argparse.ArgumentParser(prog=CLI, description=DESCRIPTION)
-    parser.add_argument('experiment_dir', type=Path, help='root directory for experiment')
     parser.add_argument('-v', '--version', action='version', version=VERSION)
     subs = parser.add_subparsers(dest='command', metavar="<command>")
 
     # Pipeline parser
     info = 'run the EA-ML analysis'
     sub = subs.add_parser('run', help=info)
-
+    sub.add_argument('experiment_dir', type=Path, help='root directory for experiment')
     sub.add_argument('data', type=Path, help='VCF file annotated by ANNOVAR and EA, or compressed sparse matrix')
     sub.add_argument('samples', type=Path,
                         help='comma-delimited file with VCF sample IDs and corresponding disease status (0 or 1)')
     sub.add_argument('gene_list', type=Path, help='single-column list of genes to test')
     sub.add_argument('-t', '--threads', type=int, default=1, help='number of threads to run Weka on')
-    sub.add_argument('-r', '--seed', type=int, default=111, help='random seed for generating KFold samples')
+    sub.add_argument('-s', '--seed', type=int, default=111, help='random seed for generating KFold samples')
     sub.add_argument('-k', '--kfolds', type=int, default=10, help='number of folds for cross-validation')
 
     # TODO: move shuffle code
     # Shuffle experiment parser
     #info = 'analyze significance of MCC scores through random shuffling'
     #sub = subs.add_parser('shuffle')
+    #sub.add_argument('experiment_dir', type=Path, help='root directory for experiment')
 
     # Visualize parser
     info = 'visualize results of EA-ML analysis'
     sub = subs.add_parser('visualize', help=info)
+    sub.add_argument('experiment_dir', type=Path, help='root directory for experiment')
     sub.add_argument('--dpi', default=150, help='DPI for output figures')
     sub.add_argument('-o', '--output', type=Path, help='location to output figures')
     sub.add_argument('-p', '--prefix', default='maxMCC', help='prefix for outfit files')
