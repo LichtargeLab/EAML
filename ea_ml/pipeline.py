bbe9aa0a445dad839858c102e855ab96beceeab4
Dillon Shapiro <drshapir@bcm.edu>
2021-02-04 12:47:27 -0600

    streamline gene parsing to ML flow

--- ea_ml/pipeline.py
+++ ea_ml/pipeline.py
@@ -4,8 +4,9 @@ import datetime
 import os
 import shutil
 import time
-from itertools import product
 from pathlib import Path
+from joblib import delayed, Parallel
+from tqdm import tqdm
 
 import numpy as np
 import pandas as pd
@@ -13,8 +14,9 @@ from pkg_resources import resource_filename
 from scipy import stats
 from statsmodels.stats.multitest import multipletests
 
-from .vcf import parse_vcf
-from .weka_wrapper import run_weka
+from .vcf import parse_gene
+from .weka_wrapper import eval_gene
+# TODO: finish and update docstrings
 
 
 class Pipeline(object):
@@ -25,13 +27,8 @@ class Pipeline(object):
         data_fn (Path): filepath to data input file (either a VCF or multi-indexed DataFrame)
         targets (Series): Array of target labels for training/prediction
         reference (DataFrame): reference background for genes
-        matrix (DesignMatrix): Object for containing feature information for each gene and sample
-        clf_info (DataFrame): A DataFrame mapping classifier names to their corresponding Weka object names and
-            hyperparameters
         kfolds (int): Number of folds for cross-validation
     """
-    feature_names = ('D1', 'D30', 'D70', 'R1', 'R30', 'R70')
-    ft_cutoffs = list(product((1, 2), (1, 30, 70)))
     class_params = {
         'PART': '-M 5 -C 0.25 -Q 1',
         'JRip': '-F 3 -N 2.0 -O 2 -S 1 -P',
@@ -44,27 +41,67 @@ class Pipeline(object):
         'MultilayerPerceptron': '-L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a'
     }
 
-    def __init__(self, expdir, data_fn, sample_targets, reference, n_jobs=1, kfolds=10):
-        self.n_jobs = n_jobs
-        self.expdir = expdir
-        self.data_fn = data_fn
-        self.targets = sample_targets
-        self.reference = reference
+    def __init__(self, expdir, data_fn, targets_fn, reference='hg19', n_jobs=1, kfolds=10, seed=111,
+                 weka_path='/opt/weka', min_af=None, max_af=None, af_field='AF', include_X=False, write_data=False):
+        # data arguments
+        self.expdir = expdir.expanduser().resolve()
+        self.data_fn = data_fn.expanduser().resolve()
+        self.targets = pd.read_csv(targets_fn, header=None, dtype={0: str, 1: int}).set_index(0).squeeze().sort_index()
+        self.reference = load_reference(reference, include_X=include_X)
+
+        # config arguments
         self.kfolds = kfolds
+        self.seed = seed
+        self.weka_path = weka_path
+        self.min_af = min_af
+        self.max_af = max_af
+        self.af_field = af_field
+        self.n_jobs = n_jobs
+        self.write_data = write_data
 
-    def compute_matrix(self, min_af=None, max_af=None, af_field='AF'):
+    def run(self):
+        gene_results = Parallel(n_jobs=self.n_jobs)(
+            delayed(self.eval_gene)(gene) for gene in tqdm(self.reference.index.unique())
+        )
+        self.report_results(gene_results)
+
+    def compute_gene_dmatrix(self, gene):
         """Computes the full design matrix from an input VCF"""
-        self.matrix = 1 - parse_vcf(self.data_fn, self.reference, list(self.targets.index), n_jobs=self.n_jobs,
-                                    min_af=min_af, max_af=max_af, af_field=af_field)
+        gene_reference = self.reference.loc[gene]
+        dmatrix = parse_gene(self.data_fn, gene, gene_reference, self.targets.index, min_af=self.min_af,
+                             max_af=self.max_af, af_field=self.af_field)
+        if self.write_data:
+            dmatrix.to_hdf(self.expdir / 'dmatrices.h5', key=gene, complevel=5, complib='zlib', format='fixed')
+        return dmatrix
 
-    def load_matrix(self):
-        """Load precomputed matrix with multi-indexed columns"""
-        self.matrix = pd.read_csv(self.data_fn, header=[0, 1], index_col=0)
+    def eval_gene(self, gene):
+        """
+        Parses input data for a given gene and evaluates it using Weka
 
-    def run_weka_exp(self, seed=None):
-        """Wraps call to weka_wrapper functions"""
-        run_weka(self.expdir, self.matrix, self.targets, self.n_jobs, self.clf_info, seed=seed,
-                 n_splits=self.kfolds)
+        Args:
+            gene (str): HGSC gene symbol
+
+        Returns:
+            dict(float): Mapping of classifier to MCC from cross validation
+        """
+        if self.expdir.suffix == '.h5':
+            hdf_fn = self.expdir / 'dmatrices.h5'
+            gene_dmatrix = pd.read_hdf(hdf_fn, key=gene)
+        else:
+            gene_dmatrix = self.compute_gene_dmatrix(gene)
+        mcc_results = eval_gene(gene, gene_dmatrix, self.targets, self.class_params, seed=self.seed, cv=self.seed,
+                                expdir=self.expdir, weka_path=self.weka_path)
+        return gene, mcc_results
+
+    def report_results(self, gene_results):
+        """
+        Summarize and rank gene scores
+
+        Args:
+            gene_results (list): Results of Weka fitting, in the format of (gene, score dict)
+        """
+        # TODO: finish reporting refactor
+        pass
 
     def summarize_experiment(self):
         """Combines results from Weka experiment files"""
@@ -106,11 +143,9 @@ class Pipeline(object):
         mean_stats = compute_stats(meanMCC_df, ensemble_type='mean')
         mean_stats.to_csv(self.expdir / 'meanMCC-results.nonzero-stats.csv')
 
-    def cleanup(self, keep_matrix=False):
-        """Deletes intermediate worker files and tmp directory."""
+    def cleanup(self):
+        """Cleans tmp directory"""
         shutil.rmtree(self.expdir / 'tmp/')
-        if keep_matrix:
-            self.matrix.to_csv(self.expdir / 'design-matrix.csv.gz')
 
 
 def compute_stats(results_df, ensemble_type='mean'):
@@ -123,7 +158,7 @@ def compute_stats(results_df, ensemble_type='mean'):
     return nonzero
 
 
-def _load_reference(reference, X_chrom=False):
+def load_reference(reference, include_X=False):
     if reference == 'hg19':
         reference_fn = resource_filename('ea_ml', 'data/hg19-refGene.protein-coding.txt')
     elif reference == 'hg38':
@@ -131,41 +166,22 @@ def _load_reference(reference, X_chrom=False):
     else:
         reference_fn = reference
     reference_df = pd.read_csv(reference_fn, sep='\t', index_col='name2')
-    if X_chrom is False:
+    if include_X is False:
         reference_df = reference_df[reference_df.chrom != 'chrX']
     return reference_df
 
 
-def run_ea_ml(exp_dir, data_fn, sample_fn, reference='hg19', cpus=1, seed=111, kfolds=10, keep_matrix=False,
+def run_ea_ml(exp_dir, data_fn, targets_fn, reference='hg19', cpus=1, seed=111, kfolds=10, write_data=False,
               include_X=False, min_af=None, max_af=None, af_field='AF'):
-    # check for JAVA_HOME
-    assert os.environ['JAVA_HOME'] is not None
-
     start = time.time()
 
-    # load input data
-    exp_dir = exp_dir.expanduser().resolve()
-    data_fn = data_fn.expanduser().resolve()
-    samples = pd.read_csv(sample_fn, header=None, dtype={0: str, 1: int}).set_index(0).squeeze()
-    reference_df = _load_reference(reference, X_chrom=include_X)
-
     # initialize pipeline
-    pipeline = Pipeline(exp_dir, data_fn, samples, reference_df, n_jobs=cpus, kfolds=kfolds)
-    # either compute design matrix from VCF or load existing one
-    if '.vcf' in str(data_fn):
-        pipeline.compute_matrix(min_af=min_af, max_af=max_af, af_field=af_field)
-    else:
-        pipeline.load_matrix()
-    print('Design matrix loaded.')
-
-    print('Running experiment...')
-    pipeline.run_weka_exp(seed=seed)
-    print('Scoring results...')
-    pipeline.summarize_experiment()
-    pipeline.write_results()
+    pipeline = Pipeline(exp_dir, data_fn, targets_fn, reference=reference, n_jobs=cpus, kfolds=kfolds, seed=seed,
+                        min_af=min_af, max_af=max_af, af_field=af_field, include_X=include_X, write_data=write_data)
+    pipeline.run()
     print('Gene scoring completed. Analysis summary in experiment directory.')
 
-    pipeline.cleanup(keep_matrix=keep_matrix)
+    pipeline.cleanup()
     end = time.time()
     elapsed = str(datetime.timedelta(seconds=end - start))
     print(f'Time elapsed: {elapsed}')
