64241a4efcdf8ee96e85bb63d90eb401a43a9c43
Dillon Shapiro <drshapir@bcm.edu>
2023-01-11 13:45:46 -0600

    bump version to 1.3.0

--- eaml/__init__.py
+++ eaml/__init__.py
@@ -1,7 +1,7 @@
 """Package for EAML"""
 
 __project__ = 'EAML'
-__version__ = '1.2.7'
+__version__ = '1.3.0'
 
 VERSION = f'{__project__} v{__version__}'
 DESCRIPTION = 'A pipeline for ranking genetic complex disease risk factors based on WES data.'
